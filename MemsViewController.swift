bec4f06cad662f66d8cf647c41021092c09e6080
Eugenia <pyssiki@yandex.ru>
2022-03-29 21:04:13 +0500

    new code and avatars

--- MemsViewController.swift
+++ MemsViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 class MemsViewController: UIViewController {
     
     
-    @IBOutlet var memsImage: UIImageView!
+    @IBOutlet var avatarImage: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-       
+    let avatars = ["Avatar1.jpeg", "Avatar2.jpeg",
+                   "Avatar3.jpeg", "Avatar4.jpeg",
+                   "Avatar5.jpeg", "Avatar6.jpeg"]
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    
 
+    @IBAction func changeAvatarPressed(_ sender: UIButton) {
+        avatarImage.image = UIImage(named: avatars[Int(arc4random_uniform(UInt32(avatars.count)))]) // рандомно присваиваем UIImageView один из аватаров из маассива avatars
+    }
 }
+
+
+/*memsImage.image = UIImage(named: "avatars \(arc4random_uniform(5) + 1).jpeg")
+еще один вариант*/
+
+
--- WelcomeViewController.swift
+++ WelcomeViewController.swift
@@ -13,12 +13,65 @@ class WelcomeViewController: UIViewController {
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var enterButton: UIButton!
     
+    let tips = Tips.getTips()
+    let developerList = Developer.getDeveloper()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initialLabel.text = """
+Чтобы получить крутой совет для
+программиста, введи свое имя!
+"""
+        enterButton.setTitle("Lets Go", for: .normal)
+        enterButton.backgroundColor = .gray
+        enterButton.layer.cornerRadius = 10
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let tabBarController = segue.destination as? UITabBarController else { return }
+        guard let viewControllers = tabBarController.viewControllers else { return }
         
+        viewControllers.forEach {
+            if let tipsChooseVC = $0 as? TipsChooseViewController {
+                tipsChooseVC.tips = tips
+                tipsChooseVC.name = nameTextField.text
+            } else if let navigationVC = $0 as? UINavigationController {
+                let developerListVC = navigationVC.topViewController as! DevelopersListViewController
+                developerListVC.developers = developerList
+            }
+        }
     }
     
     @IBAction func enterButtonPressed() {
+        if nameTextField.text == "" {
+            showAlert(title: "Введите ваше имя!", message: "Заклинаю во имя света!", textField: nameTextField)
+        }
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        nameTextField.text = ""
     }
 }
+
+extension WelcomeViewController {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+    
+    extension WelcomeViewController: UITextFieldDelegate {
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            super.touchesBegan(touches, with: event)
+            view.endEditing(true)
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            enterButtonPressed()
+                performSegue(withIdentifier: "tipsChooseVC", sender: nil)
+            return true
+            }
+        }
